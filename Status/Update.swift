5f1f630753264e30d3f18371afbecb30434c3bcc
amkane <kaneam@gmail.com>
2015-06-08 19:12:26 -0700

    add models

---
+++ Status/Update.swift
@@ -0,0 +1,15 @@
+//
+//  Update.swift
+//  Status
+//
+//  Created by Alex Kane on 6/8/15.
+//  Copyright (c) 2015 Alex Kane. All rights reserved.
+//
+
+import Foundation
+
+class Update {
+    var user: User?
+    var text: String?
+    var date: NSDate?
+}
---
+++ Status/User.swift
@@ -0,0 +1,17 @@
+//
+//  User.swift
+//  Status
+//
+//  Created by Alex Kane on 6/8/15.
+//  Copyright (c) 2015 Alex Kane. All rights reserved.
+//
+
+import Foundation
+
+class User {
+    var name: String?
+    var bio: String?
+    var userName: String?
+    var link: String?
+    var city: String?
+}
